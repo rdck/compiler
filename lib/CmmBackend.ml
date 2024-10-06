b956bc66bb18bd19bb33929fe5a9f008a208c671
River Dillon <river@k-monk.org>
2024-10-06 02:12:23 -0400

    make naming scheme more explicit

--- lib/CmmBackend.ml
+++ lib/CmmBackend.ml
@@ -4,19 +4,36 @@ open Prelude
 module S = TAC (* source *)
 module T = Cmm (* target *)
 
-let allocation_pointer = "aptr"
+(* naming scheme *)
+let name_z64 = "int64_t"
+let name_register index = sprintf "r%d" index
+let name_type index = sprintf "T%d" index
+let name_tag_type index = sprintf "T%dTag" index
+let name_eval index = sprintf "apply_t%d" index
+let name_environment_type index = sprintf "F%d" index
+let name_lambda t f = sprintf "%s_%s" (name_type t) (name_environment_type f)
+let name_argument = "arg"
+let name_allocation = "aptr"
+let name_environment = "env"
+let name_counter = "rc"
+let name_tag = "tag"
+let name_void = "void"
+let name_closure = "fp"
 
-let register_name = function
-  | S.Reg idx -> sprintf "r%d" idx
-  | S.Arg -> "arg"
-  | S.Env id -> sprintf "env->%s" id (* hack *)
+let register_var = function
+  | S.Reg index -> T.Var (name_register index)
+  | S.Arg -> T.Var name_argument
+  | S.Env id -> T.Arrow (T.Var name_environment, id)
 
-let var r = T.Var (register_name r)
-let ass r = T.Assignable (var r)
+let register_index_exn = function
+  | S.Reg index -> index
+  | _ -> failwith "expected register"
 
-let filter_store_register r' = function
-  | S.Store (r, t, _) ->
-      if [%equal: S.register] r r' then Some t else None
+let register_value r = T.Assignable (register_var r)
+
+(* extract the destination type of a store instruction *)
+let store_type r = function
+  | S.Store (r', t, _) -> if [%equal: S.register] r r' then Some t else None
   | _ -> None
 
 let compile_program source =
@@ -38,17 +55,10 @@ let compile_program source =
   (* lookup a type's index via the above map *)
   let lookup_type_index = Map.find_exn type_to_index in
 
-  let type_name index     = sprintf "T%d" index in
-  let env_name index      = sprintf "F%d" index in
-  let type_tag_name index = sprintf "T%dTag" index in
-  let enum_item_name type_index function_index =
-    sprintf "%s_%s" (type_name type_index) (env_name function_index) in
-  let application_name index = sprintf "apply_t%d" index in
-
   let atomic_type t =
     match t with
-    | STLC.Z64 -> T.TypeSymbol "int64_t"
-    | STLC.Arrow _ -> T.TypeSymbol (type_name (lookup_type_index t)) in
+    | STLC.Z64 -> T.TypeSymbol name_z64
+    | STLC.Arrow _ -> T.TypeSymbol (name_type (lookup_type_index t)) in
 
   (* map from function index to environment type *)
   let environment_map =
@@ -56,8 +66,8 @@ let compile_program source =
     let environment def =
       let atomicize { name ; value } = { name ; value = atomic_type value } in
       let reference_counter = {
-        name = "rc" ;
-        value = T.TypeSymbol "int64_t" ;
+        name = name_counter;
+        value = T.TypeSymbol name_z64;
       } in
       T.Structure (reference_counter :: (List.map def.S.env ~f:atomicize)) in
 
@@ -73,31 +83,11 @@ let compile_program source =
   (* a list of structs representing each function environment *)
   let environments =
     let alist = Map.to_alist environment_map in
-    let f (key, data) = {
-      name = env_name key ;
+    let bind (key, data) = {
+      name = name_environment_type key ;
       value = data ;
     } in
-    List.map alist ~f:f in
-
-  (*
-  (* a list of structs representing each function environment *)
-  let environments =
-
-    let environment S.{ env ; arg ; body = _ ; return_type = _ } =
-      let atomicize { name ; value } = { name ; value = atomic_type value } in
-      let reference_counter = {
-        name = "rc" ;
-        value = T.TypeSymbol "int64_t" ;
-      } in
-      T.Structure (reference_counter :: (List.map env ~f:atomicize)) in
-
-    let environment_binding (index, definition) = {
-      name = env_name index ;
-      value = environment definition ;
-    } in
-
-    List.map function_bindings ~f:environment_binding in
-  *)
+    List.map alist ~f:bind in
 
   (* filter functions by type *)
   let functions_of_type t =
@@ -122,8 +112,8 @@ let compile_program source =
   let function_enums =
 
     let to_binding (type_index, function_indices) =
-      let ids = List.map function_indices ~f:(enum_item_name type_index) in
-      { name = type_tag_name type_index ; value = T.Enumeration ids } in
+      let ids = List.map function_indices ~f:(name_lambda type_index) in
+      { name = name_tag_type type_index ; value = T.Enumeration ids } in
 
     List.map type_index_with_functions ~f:to_binding in
   
@@ -132,14 +122,12 @@ let compile_program source =
     let structure t =
       let index = lookup_type_index t in
       let def = T.Structure [
-        { name = "tag" ; value = T.TypeSymbol (type_tag_name index) } ;
-        { name = "env" ; value = T.Pointer (T.TypeSymbol "void") } ;
+        { name = name_tag         ; value = T.TypeSymbol (name_tag_type index)  } ;
+        { name = name_environment ; value = T.Pointer (T.TypeSymbol name_void)  } ;
       ] in
-      { name = type_name index ; value = def } in
+      { name = name_type index ; value = def } in
 
     List.map function_types ~f:structure in
-
-  (* code gen *)
 
   let compile_op = function
     | STLC.Add -> T.Add
@@ -151,48 +139,51 @@ let compile_program source =
 
     let compile_expression = function
       | S.Lit i -> T.Lit i
-      | S.Bin (op, lhs, rhs) -> T.Bin (compile_op op, ass lhs, ass rhs)
+      | S.Bin (op, lhs, rhs) ->
+          T.Bin (compile_op op, register_value lhs, register_value rhs)
       | S.Closure _ -> failwith "UNREACHABLE"
       | S.Call (f, x) ->
           let ft_index' = lookup_type_index (register_type f) in
-          T.Call (application_name ft_index', [ ass f ; ass x ]) in
+          T.Call (name_eval ft_index', [ register_value f ; register_value x ]) in
 
     let compile_instruction =
       function
         | S.Store (dest, t, Closure (fidx, args)) ->
-            let rname = register_name dest in
+            let register_name = name_register (register_index_exn dest) in
+            let register = register_var dest in
             let tidx = lookup_type_index t in
-            let register_decl = T.Declare (rname, atomic_type t) in
-            let tag_assignment = T.Assign (
-              T.Dot (T.Var rname, "tag"),
-              T.Assignable (T.Var (enum_item_name tidx fidx))
-            ) in
-            let eptr_type = T.Pointer (T.TypeSymbol (env_name fidx)) in
-            let declaration = T.Declare (allocation_pointer, eptr_type) in
+            let eptr_type = T.Pointer (T.TypeSymbol (name_environment_type fidx)) in
+            let declaration = T.Declare (name_allocation, eptr_type) in
+            (* TODO: encode malloc structure properly *)
             let malloc = T.Assign (
-              T.Var allocation_pointer,
-              T.Assignable (T.Var "malloc(sizeof(*aptr))") (* fix later *)
+              T.Var name_allocation,
+              T.Assignable (T.Var (sprintf "malloc(sizeof(*%s))" name_allocation))
             ) in
             let env_assignment = T.Assign (
-              T.Dot (T.Var rname, "env"),
-              T.Assignable (T.Var "aptr")
+              T.Dot (register, name_environment),
+              T.Assignable (T.Var name_allocation)
             ) in
             let arg_assignment =
               let assign_arg name value =
-                T.Assign (
-                  T.Arrow (T.Var "aptr", name),
-                  T.Assignable (T.Var (register_name value))
-                ) in
-              let env_names = get_environment_names fidx in
-              List.map2_exn env_names args ~f:assign_arg in
-            let block = T.Block (declaration :: malloc :: env_assignment :: arg_assignment) in
-            [ register_decl ; tag_assignment ; block ]
+                T.Assign (T.Arrow (T.Var name_allocation, name), register_value value) in
+              let environment_names = get_environment_names fidx in
+              List.map2_exn environment_names args ~f:assign_arg in
+            let block = declaration :: malloc :: env_assignment :: arg_assignment in
+            [
+              T.Declare (register_name, atomic_type t) ;
+              T.Assign (
+                T.Dot (register, name_tag),
+                T.Assignable (T.Var (name_lambda tidx fidx))
+              ) ;
+              T.Block block ;
+            ]
         | S.Store (dest, t, v) ->
-            let rname = register_name dest in
-            let register_decl = T.Declare (rname, atomic_type t) in
-            let assignment = T.Assign (T.Var rname, compile_expression v) in
+            let register_name = name_register (register_index_exn dest) in
+            let register = register_var dest in
+            let register_decl = T.Declare (register_name, atomic_type t) in
+            let assignment = T.Assign (register, compile_expression v) in
             [ register_decl ; assignment ]
-        | S.Return r -> [T.Return (T.Assignable (T.Var (register_name r)))] in
+        | S.Return r -> [T.Return (register_value r)] in
 
     List.map instructions ~f:compile_instruction in
 
@@ -212,30 +203,37 @@ let compile_program source =
         let filter_arg_type id { name ; value } =
           if String.equal id name then Some value else None in
         let get_register_type = function
-          | S.Reg _ as r -> List.find_map_exn fdef.S.body ~f:(filter_store_register r)
+          | S.Reg _ as r -> List.find_map_exn fdef.S.body ~f:(store_type r)
           | S.Arg -> domain
           | S.Env id -> List.find_map_exn fdef.S.env ~f:(filter_arg_type id) in
 
-        let env_decl = T.Declare ("env", Pointer (TypeSymbol (env_name fidx))) in
-        let env_defi = T.Assign (T.Var "env", T.Assignable (T.Var "fp.env")) (* hack *) in
+        let env_decl = T.Declare (
+          name_environment,
+          Pointer (TypeSymbol (name_environment_type fidx))
+        ) in
+
+        let env_defi = T.Assign (
+          T.Var name_environment,
+          T.Assignable (T.Dot (T.Var name_closure, name_environment))
+        ) in
         let body = List.concat (compile_instructions fdef.S.body get_register_type) in
 
         T.{
-          tag = T.Assignable (T.Var (enum_item_name ft_index fidx)) ;
+          tag = T.Assignable (T.Var (name_lambda ft_index fidx)) ;
           body = env_decl :: env_defi :: body ;
         } in
 
       let cases = List.map functions ~f:to_case in
 
       {
-        name = application_name ft_index ;
+        name = name_eval ft_index ;
         value = T.{
           args = [
-            { name = "fp" ; value = T.TypeSymbol (type_name ft_index) ; } ;
-            { name = "arg" ; value = atomic_type domain ; } ;
+            { name = name_closure   ; value = T.TypeSymbol (name_type ft_index) ; } ;
+            { name = name_argument  ; value = atomic_type domain                ; } ;
           ] ;
           body = [
-            T.Switch (T.Assignable (T.Var "fp.tag"), cases) ;
+            T.Switch (T.Assignable (T.Dot (T.Var name_closure, name_tag)), cases) ;
           ] ;
           return_type = atomic_type codomain ;
         } ;
@@ -245,7 +243,7 @@ let compile_program source =
 
   (* duplicated logic with above *)
   let get_register_type = function
-    | S.Reg _ as r -> List.find_map_exn source.S.body ~f:(filter_store_register r)
+    | S.Reg _ as r -> List.find_map_exn source.S.body ~f:(store_type r)
     | S.Arg -> failwith "unexpected arg in main"
     | S.Env _ -> failwith "unexpected env in main" in
   let main = List.concat (compile_instructions source.S.body get_register_type) in
