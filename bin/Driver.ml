cc9625493f3d8241608c2bdac5a1cc5a4959b590
River Dillon <river@k-monk.org>
2024-10-24 19:21:02 -0400

    wip

--- bin/Driver.ml
+++ bin/Driver.ml
@@ -1,7 +1,54 @@
 open Core
 open Compiler
 
-let compile path =
+type mode =
+  | TAC
+  | C
+
+(* stores each stage of compilation *)
+type compilation = {
+  tokens    : Token.token list  ;
+  ast       : STLC.program      ;
+  annotated : Annotated.program ;
+  lifted    : Lifted.program    ;
+  tac       : TAC.program       ;
+  cmm       : Cmm.program       ;
+  output    : string            ;
+}
+
+let compile source = failwith ""
+
+let represent =
+  failwith ""
+
+let driver mode path =
+
+  (* determine mode *)
+  let mode = match mode with
+  | Some "tac" -> TAC
+  | _ -> C in
+
+  (* check path *)
+  match Sys_unix.is_file_exn path with
+  | true ->
+    let compilation = compile path in
+    printf "%s\n" (represent mode compilation)
+  | false ->
+    printf "invalid path\n"
+
+let command =
+  Command.basic
+  ~summary:"compiler"
+  ~readme:(fun () -> "more about the compiler")
+  (
+    let%map_open.Command mode = flag "-c" (optional string) ~doc:"compilation mode"
+    and path = anon ("path" %: string) in
+    fun () -> driver mode path
+  )
+
+
+(*
+let compile source =
   match Sys_unix.is_file_exn path with
   | true ->
       let content = In_channel.read_all path in
@@ -16,7 +63,13 @@ let compile path =
       printf "invalid path\n"
 
 let () = 
+
   let command =
+
+    let param_handler path () =
+      match Sys_unix.is_file_exn path with
+      | true -> compile (In_channel.read_all path)
+
     let readme () = "compiler readme" in
     let param_spec = Command.Param.(anon ("path" %: string)) in
     let param_handler path () = compile path in
@@ -24,4 +77,6 @@ let () =
     ~summary:"recurse center simply typed lambda calculus compiler"
     ~readme:readme
     (Command.Param.map param_spec ~f:param_handler) in
+
   Command_unix.run command
+*)
